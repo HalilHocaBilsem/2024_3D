d628dae513b5f3bb9ff0f0c0d045e53a6732a730
HalilHocaBilsem <149094600+HalilHocaBilsem@users.noreply.github.com>
2025-03-17 18:16:02 +0300

    Update Zombi.cs

--- Zombi.cs
+++ Zombi.cs
@@ -3,54 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// Zombi nesnesinde çalıştırılacak koddur.
-/// Zombinin hasar alması için fırlatılan topun top tagine sahip olması gerekir.
-/// Zombi kafası scripte tanımlanmalıdır.
+/// Zombi nesnesi üzerinde çalışacak scripttir. 
+/// Zombi kafasının scripte tanımlanması gerekmektedir. 
+/// Zombinin hasar alması için fırlatılan topların mermi tagine sahip olması gerekir.
 /// </summary>
 public class Zombi : MonoBehaviour
 {
-
-    public GameObject kafa;
-    public int Can = 100;
+  
+    public int Can = 100; 
     public bool CanliMi = true;
 
+    public GameObject Kafa;
+    void Start()
+    {
+        CanliMi = true;
+        Can = 100;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "mermi")
+        {
+            Can = Can - 10;
+
+            if (Can <= 0)
+            {
+                CanliMi = false;
+                var rb = GetComponent<Rigidbody>();
+                rb.constraints = RigidbodyConstraints.None;
+            }
+        }
+    }
 
 
     void Update()
     {
-        if (CanliMi==false)
+        if (CanliMi == true)
         {
-            return;
-        }
+            Vector3 hedef = Vector3.Lerp(this.gameObject.transform.position, Camera.main.transform.position, 0.5f * Time.deltaTime);
+            this.transform.position = hedef;
 
-        Vector3 directionToCamera = Camera.main.transform.position - transform.position;
-
-        // Sadece Y ekseninde dönme işlemi yapılacak
-        directionToCamera.y = 0;
-
-        // Objeyi kameraya doğru yavaşça döndür
-        Quaternion targetRotation = Quaternion.LookRotation(directionToCamera);
-
-        // Yavaşça dönüş
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 50 * Time.deltaTime);
-
-
-        var ortaNokta =Vector3.Lerp(this.transform.position, Camera.main.transform.position,0.9f* Time.deltaTime);
-        this.transform.position=ortaNokta;
-    }
-
-   
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag=="top")
-        {
-            Can -= 5;
-            if (Can<=0)
-            {
-                CanliMi = false;
-        var rb=GetComponent<Rigidbody>();
-        rb.constraints = RigidbodyConstraints.None;
-            }
+            Kafa.transform.LookAt(Camera.main.transform.position);
         }
     }
 }
